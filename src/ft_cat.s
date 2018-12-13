70bae57b372014d5255454ca581890c591450f6d
Yoan GARROT <ygarrot@e3r5p7.42.fr>
2018-12-13 15:38:42 +0100

    lame

--- src/ft_cat.s
+++ src/ft_cat.s
@@ -1,35 +1,40 @@
 %define MACH_SYSCALL(nb) 0x2000000 | nb
 %define READ 3
 %define WRITE 4
-%define STDIN 1 
+%define STDIN 1
 
 section .data
 	buf times 255 db 0
 	bufsize equ $ - buf
 
 section .text
-global ft_cat
+	global ft_cat
 
 ft_cat:
 	enter 0, 0
-	push rdi
+	mov r14, rdi
+	lea rsi, [rel buf]
 	;read(fd, buff_size, buf)
 .read:
-	lea rsi, [rel buf]
+	mov rdi, r14
 	mov rdx, bufsize
 	mov rax, MACH_SYSCALL(READ)
 	syscall
+	jc .err
 	cmp rax, 0
-	jle .ret
+	je .ret
 
 	;write(fd, buf, buff_size)
 .write:
-	mov rdx, rax
 	mov rdi, STDIN
+	mov rdx, rax
 	mov rax, MACH_SYSCALL(WRITE)
 	syscall
-	pop rdi
+	jc .err
 	jmp .read
+
+.err:
+	mov rax, 1
 
 .ret:
 	leave
